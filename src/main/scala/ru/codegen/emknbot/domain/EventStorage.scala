aa4355f30c723818e1c381d9f821b07e09642da4
IdeaSeeker <stroganov.n.36@gmail.com>
2021-04-28 20:55:55 +0300

    + MapEventStorage

--- src/main/scala/ru/codegen/emknbot/domain/EventStorage.scala
+++ src/main/scala/ru/codegen/emknbot/domain/EventStorage.scala
@@ -4,9 +4,9 @@ import ru.codegen.emknbot.domain.userinfo._
 
 trait EventStorage[F[_]] {
 
-    def add(event: EmknEvent): F[Unit]
+    def add(userId: Id, event: EmknEvent): F[Unit]
 
-    def remove(event: EmknEvent): F[Unit]
+    def remove(userId: Id, event: EmknEvent): F[Unit]
 
     def getByUserId(id: Id): F[List[EmknEvent]]
 }
